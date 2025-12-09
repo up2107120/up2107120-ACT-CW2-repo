1ed88a5147f309d21725aa854342d703e9ec1081
Adam Williams <up2107120@myport.ac.uk>
2025-12-09 21:32:49 +0000

    functions.py file

---
+++ py/functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler , OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import plot_tree
+from sklearn import tree
+import torch
+import torch.nn as nn 
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader, Dataset 
+import optuna
