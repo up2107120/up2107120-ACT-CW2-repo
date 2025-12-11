8cfa3d049c53da303a246b398121d83292c9dd65
Adam Williams <up2107120@myport.ac.uk>
2025-12-11 13:20:11 +0000

    functions.py updates again with directory changes

--- py/functions.py
+++ py/functions.py
@@ -15,6 +15,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader, Dataset 
 import os
-def load_dataset(filename ='space_missions_dataset.csv',data_dir = 'data'):
+def load_dataset(filename ='space_missions_dataset.csv',data_dir = 'space-missions-dataset'):
   path = os.path.join(data_dir,filename)
   return pd.read_csv(path)
